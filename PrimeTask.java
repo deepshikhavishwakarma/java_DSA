ba2ebed9ffb56bfeee28a2520ac737031df607b0
deepshikhavishwakarma <deepshikha2000deep@gmail.com>
2023-07-18 15:42:14 +0530

    Add PrimeTask file

---
+++ PrimeTask.java
@@ -0,0 +1,17 @@
+class Prime {
+
+    public static void isPrime(int x) {
+        if (x >= 2) {
+
+        } else {
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();
+
+        // sc.close();
+    }
+}
